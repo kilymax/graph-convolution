df1770b787f507c68e5bdf789256e130baac4210
lostie21yo <leon.coxsw@gmail.com>
2023-02-15 13:27:59 +0300

    Class Main, open_file, param_selection

--- script.py
+++ script.py
@@ -1,13 +1,50 @@
-import csv
+import pandas as pd
+import numpy as np
+import os
+from tkinter import *
+from tkinter import filedialog as fd
 
-count = 0
+class Main(Tk):
 
-with open('test.csv', 'r', newline='') as csvfile:
-    reader = csv.reader(csvfile, dialect='excel' , delimiter=' ', quotechar='|')
-    for row in reader:
-        print(row)
-        count += 1
+    def __init__(self):
+        super().__init__()
+        self.button1 = Button(self, text='Выбрать\nCSV-файл', width=16, height=2, 
+                              font='Verdana 12', command=self.open_file)
+        self.button1.place(x=20, y=20)
+
+    # функция с основным функционалом
+    def open_file(self):
+
+        # выбор и открытие CSV-файла
+        # image_path = fd.askopenfilename()
+        image_path = 'test.csv'
+        df = pd.read_csv(image_path, delimiter='\t', encoding='utf-16')
+        cols = df.columns
+        colscount = len(cols)
+        rows = df[cols[1]][1:]
+        rowscount = len(rows)
+
+        self.label1 = Label(text=f'Параметров: {colscount}\nЗначений: {rowscount}', 
+                            width=16,font='Verdana 12')
+        self.label1.place(x=20, y=80)
+
+        self.param_selection()
+
+    def param_selection(self):
+        selection_window = Tk()
+        selection_window.geometry('600x600')
+        selection_window.title('Выбор параметров')
+        selection_window['bg'] = 'white'
+
+        selection_window.mainloop()
 
 
-print(type(reader))
-print(count)
+file_dir = os.path.split(__file__)[0] + '\\'
+if __name__ == "__main__":
+    main = Main()
+    main.geometry('1280x720')
+    main.title('CSV Smoother')
+    main['bg'] = 'white'
+
+
+    main.mainloop()
