2cf552047fa74cc88913116f7fbdd2b2f61b160c
lostie21yo <leon.coxsw@gmail.com>
2023-02-15 16:27:20 +0300

    2 frames, create_plots started

--- script.py
+++ script.py
@@ -3,14 +3,32 @@ import numpy as np
 import os
 from tkinter import *
 from tkinter import filedialog as fd
+from matplotlib import pyplot as plt
 
 class Main(Tk):
 
     def __init__(self):
         super().__init__()
-        self.button1 = Button(self, text='Выбрать\nCSV-файл', width=16, height=2, 
+
+        paramindexlist = IntVar()
+
+        self.leftframe = Frame(self, bg='#bababa')
+        self.leftframe.pack(anchor=NW, side=LEFT, expand=1, fill=Y)
+
+        self.labelframe1 = LabelFrame(self.leftframe, 
+                            font=font, bg='#bababa')
+        self.labelframe1.pack(padx=5, pady=5, ipadx=5, ipady=5)
+
+        self.button1 = Button(self.labelframe1, text='Выбрать CSV-файл', width=22, height=1, 
                               font='Verdana 12', command=self.open_file)
-        self.button1.place(x=20, y=20)
+        self.button1.pack(side=TOP, padx=5, pady=5)
+
+        self.label1 = Label(self.labelframe1, text='', width=20,
+                            height=2, bg='#bababa', font=font)
+        self.label1.pack()
+
+        self.labelframe2 = LabelFrame(self.leftframe, text='Выбор параметров', font=font, bg='#bababa')
+        self.labelframe2.pack(expand=1, fill=Y, padx=5, pady=5, ipadx=5, ipady=5)
 
     # функция с основным функционалом
     def open_file(self):
@@ -24,27 +42,34 @@ class Main(Tk):
         rows = df[cols[1]][1:]
         rowscount = len(rows)
 
-        self.label1 = Label(text=f'Параметров: {colscount}\nЗначений: {rowscount}', 
-                            width=16,font='Verdana 12')
-        self.label1.place(x=20, y=80)
+        # Вывод информации о данных
+        self.label1.config(text=f'Параметры: {colscount}\nСтроки: {rowscount}')
 
-        self.param_selection()
+        # Выбор параметров для построения графиков
+        listbox = Listbox(self.labelframe2, selectmode=MULTIPLE, width=36, height=24)
+        for param in cols:
+            listbox.insert(END, param)
+        listbox.pack(ipadx=5, ipady=5, padx=5, pady=5,)
 
-    def param_selection(self):
-        selection_window = Tk()
-        selection_window.geometry('600x600')
-        selection_window.title('Выбор параметров')
-        selection_window['bg'] = 'white'
+        self.button2 = Button(self.labelframe2, text='Построить графики', width=18, height=1, 
+                              font='Verdana 12', command=self.create_plots)
+        self.button2.pack(side=TOP, padx=5, pady=5)
 
-        selection_window.mainloop()
+    def create_plots(self):
+        paramindexlist = self.listbox.curselection()
+        x = np.arange(-10, 10.01, 0.01)
+        plt.plot(x, x**2)
+        plt.show()
 
 
 file_dir = os.path.split(__file__)[0] + '\\'
+window_size = (1200, 600)
+font = 'Verdana 12'
 if __name__ == "__main__":
     main = Main()
-    main.geometry('1280x720')
+    main.geometry(f'{window_size[0]}x{window_size[1]}')
     main.title('CSV Smoother')
-    main['bg'] = 'white'
+    # main['bg'] = 'white'
 
 
     main.mainloop()
